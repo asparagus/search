32a7a3f7fbc38242caeeea32a85e353e92708d1d
Ariel Perez <arielperezch@gmail.com>
2021-09-07 04:41:28 -0700

    Refactor transformation arguments & typing to properly reflect what it does

--- search/transformation.py
+++ search/transformation.py
@@ -1,16 +1,17 @@
 """Contains the base class for transformating one problem to another."""
 import abc
+from typing import Any
 
 
 class Transformation(abc.ABC):
     """Base class for a transformation."""
 
     @abc.abstractmethod
-    def apply(self, problem):
+    def apply(self, problem: Any):
         """Applythe transformation on a problem."""
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def revert(self, problem):
+    def revert(self, solution: Any):
         """Revert the transformation on the problem's output."""
         raise NotImplementedError()
